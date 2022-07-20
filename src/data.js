053f68ecee4e5177a2198d7cb9609bfffe2f4da6
AuraFly <JordanJCo@gmail.com>
2022-07-20 06:52:31 -0600

    Added the rest of my projects

--- src/data.js
+++ src/data.js
@@ -5,31 +5,104 @@ export const projects = [
     description:
       "One of my first projects to showcase that I knew how to use basic HTML, CSS, and Javascript",
     image: "./passgen.png",
-    link: "https://reactbootcamp.com",
+    link: "https://aurafly.github.io/Password_Generator_Homework/",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
+    title: "Text Editor",
+    subtitle: "HTML/CSS/Javascript",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
-    link: "https://reedbarger.com",
+      "One of my first projects to showcase that I knew how to use basic HTML, CSS, and Javascript, practicing local storage and timers",
+    image: "./quiz.png",
+    link: "https://aurafly.github.io/Code_Quiz_Homework/",
   },
   {
-    title: "DevChat",
-    subtitle: "React and Firebase",
+    title: "A.T.O.M. (Another Text Output Machine)",
+    subtitle: "Node.JS, Javascript",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://jsbootcamp.com",
+      "This single-page application showcases a Node.JS featuring a IndexedDB database meeting PWA criteria.",
+    image: "./TextEditor.gif",
+    link: "https://github.com/AuraFly/PWA_Text_Editor",
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
+    title: "Social Network API",
+    subtitle: "MONGO, Mongoose ODM, Express.JS Javascript, Node.JS",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://pythonbootcamp.com",
+      " This is an API for a social network web application where users can share their thoughts, react to friends thoughts, and create a friend list. It features Express.js for routing, a MongoDB database, and the Mongoose ODM.",
+    image: "./socialnetworkapi.gif",
+    link: "https://github.com/AuraFly/A_Social_Network",
+  },
+  {
+    title: "Pepper Pods Blog",
+    subtitle: "Node.JS, Handlebars, Javascript, UIKit, MVC",
+    description:
+      "A CMS-style blog site, where users can publish their blog posts and comment on others posts as well. This project was to showcase the use of MVC, and features handlebars, sequelize, and express-session",
+    image: "./pepperp.gif",
+    link: "https://github.com/AuraFly/Pepper_Pods_Blog",
+  },
+  {
+    title: "Fit With Friends",
+    subtitle:
+      "Node.JS, Handlebars, Javascript, Express-session, MYSQL, Bootstrap",
+    description:
+      "Our second two-week project of the University of Denver bootcamp. Partnering with Cynthia Godoy, Ellen Furness, and Walker Jezek. We decided to make a fitness tracking blog",
+    image: "./FWF.gif",
+    link: "https://github.com/AuraFly/Pepper_Pods_Blog",
+  },
+  {
+    title: "Ecommerce BackEnd",
+    subtitle: "Node.JS, Javascript, MYSQL, ExpressJS, Sequelize",
+    description:
+      "Features node.js and MySQL for storage. Runs off of Node, there is no front end, so needs to be tested using a tool like insomnia. Test GET/PUT/POST/DELETE CRUD operations",
+    image: "./ecomm.gif",
+    link: "https://github.com/AuraFly/E-Commerce_Back_End",
+  },
+  {
+    title: "Employee Tracker",
+    subtitle: "Node.JS, Javascript, Inquirer, and MySql",
+    description:
+      "A command-line application I built from scratch to emulate managing a company's employee database, using Node.js, Inquirer, and MySQL. I ended up trying out some additional packages in this assignment as well like colors and sql-template-strings.",
+    image: "./employee.png",
+    link: "https://github.com/AuraFly/Employee_Tracker",
+  },
+  {
+    title: "Note Taker",
+    subtitle: "Node.JS, Javascript, HTML, CSS",
+    description:
+      "Modified starter code to create an application that can be used to write and save notes.",
+    image: "./demo.gif",
+    link: "https://github.com/AuraFly/Note_Taker",
+  },
+  {
+    title: "Team Profile Generator",
+    subtitle: "Node.JS, Javascript, CSS",
+    description:
+      "A Node.js CL application that takes in information about employees on a software engineering team, then generates an HTML webpage that displays summaries for each person.",
+    image: "./ss2.jpg",
+    link: "https://github.com/AuraFly/Team_Profile_Generator",
+  },
+  {
+    title: "Pantry Recipes",
+    subtitle: "Node.JS, Javascript, Bulma, Edemam API, Google Maps API",
+    description:
+      "Our first two-week project of the University of Denver bootcamp. Partnering with David Irving and Shane Wilmes. We decided to make a web app that can find recipes based off of ingredients and dietary restrictions.",
+    image: "./pantry.png",
+    link: "https://github.com/AuraFly/Team_Profile_Generator",
+  },
+  {
+    title: "Weather Dashboard",
+    subtitle: "Javascript, CSS, HTML, OpenWeatherMapAPI",
+    description:
+      "A dashboard that allows users to view current weather details, 5 day forcast, and retains search history using local storage!",
+    image: "./weather.png",
+    link: "https://github.com/AuraFly/Weather_Dashboard",
+  },
+  {
+    title: "Daily Planner",
+    subtitle: "Javascript, CSS, HTML",
+    description:
+      "A Daily planner that allows a user to save events for each hour of the day!.",
+    image: "./planner.gif",
+    link: "https://github.com/AuraFly/Daily_Planner",
   },
 ];
 
