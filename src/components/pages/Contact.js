166cd94b5577741da8a08aae67139b5a3faa9475
AuraFly <JordanJCo@gmail.com>
2022-07-21 11:23:17 -0600

    Fixing forms

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -27,7 +27,10 @@ export default function Contact() {
     })
       .then(() => {
         result.innerText = "Success";
-      .catch((err) => alert(err));
+      })
+      .catch((error) => {
+        result.innerText = `Failed: ${error}`;
+      });
   };
 
   return (
